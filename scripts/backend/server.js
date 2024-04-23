9e0ac7db3baeaa0a2382169b4dc114402b3b7137
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-04-23 17:48:23 +0200

    Adding initial packages

--- scripts/backend/server.js
+++ scripts/backend/server.js
@@ -6,11 +6,15 @@ const bodyparser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "/.."));
+app.use(express.static(path.join(__dirname, '/..')));
+
 app.set("view engine", "ejs");
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    res.send("Page");
+    console.log("Inside");
+    res.render("ejs/index.ejs");
 });
 
 app.listen(PORT, _ => console.log("Listening on port: " + PORT));
