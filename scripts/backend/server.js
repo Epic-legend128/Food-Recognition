0a15a51b022e2547f1bb2a8b2ff858cc7f16840d
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-04-23 18:02:47 +0200

    Some extra stuff

--- scripts/backend/server.js
+++ scripts/backend/server.js
@@ -12,9 +12,25 @@ app.use(express.static(path.join(__dirname, '/..')));
 app.set("view engine", "ejs");
 app.use(express.json());
 
+const allowedPages = ["home", "foodRecognition"];
+
 app.get("/", async (req, res) => {
     console.log("Inside");
-    res.render("ejs/index.ejs");
+    res.render("ejs/home.ejs");
 });
+
+app.get("/:id", async (req, res) => {
+    if (allowedPages.includes(req.params.id)) {
+        res.render("ejs/"+req.params.id+".ejs");
+    }
+    else {
+        res.render("ejs/pageNotFound.ejs");
+    }
+});
+
+app.get("/*", async (req, res) => {
+    res.render("ejs/pageNotFound.ejs");
+});
+
 
 app.listen(PORT, _ => console.log("Listening on port: " + PORT));
