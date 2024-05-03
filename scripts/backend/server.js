35f5829706dab646dd82abf489dbd437bb6bd2c9
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-05-03 11:31:48 +0200

    Put env

--- scripts/backend/server.js
+++ scripts/backend/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyparser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "/.."));
 app.use(express.static(path.join(__dirname, '/..')));
