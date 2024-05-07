e73f008207ca6fd2123460025e216e08c13f63ec
Bigfoot2008 <emanuele.schena@student.h-is.com>
2024-05-07 10:50:33 +0200

    New stuff

---
+++ scripts/js/aboutUS.js
@@ -0,0 +1,18 @@
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200) {
+        $('header').addClass('visible').removeClass('hidden');
+    } else {
+        $('header').addClass('hidden').removeClass('visible');
+    }
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
