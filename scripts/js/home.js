8e65f649c534e32e59a4c9a39e7cd780631df2e7
Bigfoot2008 <emanuele.schena@student.h-is.com>
2024-05-07 09:42:48 +0200

    Update home.html - home.css - home.js

--- scripts/js/home.js
+++ scripts/js/home.js
@@ -6,3 +6,13 @@ $(window).scroll(function () {
         $('header').addClass('hidden').removeClass('visible');
     }
 });
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
