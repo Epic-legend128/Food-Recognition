405c36e881c25683702402e1d2a043b5b3e16991
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-05-22 09:28:43 +0200

    Teachable machine url

--- scripts/js/foodRecognition.js
+++ scripts/js/foodRecognition.js
@@ -24,7 +24,7 @@ const constraints = {
 };
 
 const KEY = "keyForTheLocalSessionForThePredictionsMadeByTheModel";
-const URL = "<teachable-url>";
+const URL = "https://teachablemachine.withgoogle.com/models/ctWVwXRh_/";
 
 async function openCamera() {
     $("#analyze-btn").show();
